daae63d4d6cbbf89565950a79da6c331470ec26b
Roop Kumar <108384154+roopk2000@users.noreply.github.com>
2024-03-03 00:39:11 +0530

    Commit By Roop for Xcountries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css"; // Import your CSS file
 
-function App() {
+function CountriesApp() {
+  const [countries, setCountries] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  const fetchCountries = () => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch countries");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setCountries(data);
+      })
+      .catch((error) => {
+        console.error(error);
+        setError("Failed to fetch countries");
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+      {error && <p>{error}</p>}
+      {countries.length > 0 ? (
+        countries.map((country) => (
+          <div key={country.name.common} className="grid-item">
+            <img src={country.flags.svg} alt={country.name.common} />
+            <p>{country.name.common}</p>
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
 
-export default App;
+export default CountriesApp;
